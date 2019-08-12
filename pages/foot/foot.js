896efad324c69c2dd47eb7814ad9e15fcbdabc67
Mrzhzhzh <40493350+Mrzhzhzh@users.noreply.github.com>
2019-08-12 21:24:20 +0800

    test

--- pages/foot/foot.js
+++ pages/foot/foot.js
@@ -10,17 +10,41 @@ Page({
     mainData:[],
     isTouchMove:'',
     translateStyle:'',
-    isFirstLoadAllStandard:['getMainData'],
+    isFirstLoadAllStandard:['getMainData','getUserData'],
   },
 
 
   onLoad(options) {
     const self = this;
+	var now =  Date.parse(new Date())/1000;
     api.commonInit(self);
 	wx.setNavigationBarTitle({
 		title: '我的足迹'
 	});
     self.getMainData();
+	self.getUserData();
+	self.setData({
+		web_now:now
+	})
+  },
+  
+  
+  getUserData(){
+  	const self = this;
+  	const postData = {};
+  	postData.tokenFuncName = 'getProjectToken';
+  	const callback = (res)=>{
+  	  if(res.solely_code==100000){
+  	    self.data.userData = res.info.data[0]
+  	  }else{
+  	    api.showToast('网络故障','none')
+  	  }
+  	  api.checkLoadAll(self.data.isFirstLoadAllStandard,'getUserData',self);
+  	  self.setData({
+  	    web_userData:self.data.userData,
+  	  });  
+  	};
+  	api.userGet(postData,callback)
   },
 
   getMainData(){
